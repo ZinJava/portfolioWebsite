eca0e2f05fb7617247021474eb1550d7c7fb5038
ZinJava <maziniqbal2005@gmail.com>
2025-03-26 13:23:35 -0400

    working on contact form submission

--- src/main/java/zinjava/madisonportfolio/controller/ContactController.java
+++ src/main/java/zinjava/madisonportfolio/controller/ContactController.java
@@ -1,15 +1,46 @@
 package zinjava.madisonportfolio.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/contact")
 public class ContactController {
 	@PostMapping("/send")
-	public String send(@RequestParam String name, @RequestParam String email, @RequestParam String message) {
-		return "contact";
+	public ResponseEntity<String> send(@RequestBody FormData formData) {
+		return ResponseEntity.ok("Message sent successfully");
+	}
+}
+
+class FormData {
+	private String name;
+	private String email;
+	private String message;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 }
---
+++ src/main/java/zinjava/madisonportfolio/model/ContactRequest.java
@@ -0,0 +1,4 @@
+package zinjava.madisonportfolio.model;
+
+public class ContactRequest {
+}
